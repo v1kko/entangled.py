4cac53666cc57ba19fd96a53932338efd2db462c
Johan Hidding <j.hidding@esciencecenter.nl>
2025-12-01 23:58:28 +0100

    restore back to old watchdog implementation, but upgrade to git main version (to be 7.0)

--- entangled/commands/watch.py
+++ entangled/commands/watch.py
@@ -1,11 +1,37 @@
 from pathlib import Path
 from threading import Event
-import subprocess
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
 
 from ..status import find_watch_dirs
 
 from .sync import run_sync
 from .main import main
+
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.update_watched()
+
+    def update_watched(self):
+        self.watched = find_watch_dirs()
+
+    def on_any_event(self, event: FileSystemEvent):
+        if event.event_type == "opened":
+            return
+
+        if isinstance(event.src_path, bytes):
+            path = Path(event.src_path.decode("utf-8"))
+        else:
+            path = Path(event.src_path)
+
+        if path.absolute().is_relative_to(Path("./.entangled").absolute()):
+            return
+        if any(path.absolute().is_relative_to(p.absolute()) for p in self.watched):
+            run_sync()
+            # os.sync()
+        self.update_watched()
 
 
 def _watch(_stop_event: Event | None = None, _start_event: Event | None = None):
@@ -16,12 +42,22 @@ def _watch(_stop_event: Event | None = None, _start_event: Event | None = None):
     def stop() -> bool:
         return _stop_event is not None and _stop_event.is_set()
 
+    run_sync()
+
+    event_handler = EventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, ".", recursive=True)
+    observer.start()
+
     if _start_event:
         _start_event.set()
 
-    while not stop():
-        subprocess.run(["watchman-wait"] + list(find_watch_dirs()))
-        run_sync()
+    try:
+        while observer.is_alive() and not stop():
+            observer.join(0.1)
+    finally:
+        observer.stop()
+        observer.join()
 
 
 @main.command()
